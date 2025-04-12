577fe23d7702cb1209e8aef4048a3498037afe75
baoho-code <bhdesignn@gmail.com>
2025-04-12 13:51:49 +0700

    Add files via upload

--- js/pop-up-lang.js
+++ js/pop-up-lang.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Hiện popup + overlay + blur nội dung chính
-    document.getElementById('language-popup').style.display = 'block';
-    document.getElementById('language-popup-overlay').style.display = 'block';
-    document.getElementById('main-content').classList.remove('language-selected');
-  });
+  // Kiểm tra để tránh hiển thị popup khi đang ở trang ngôn ngữ
+  const path = window.location.pathname;
+  if (!path.includes('vi') && !path.includes('en')) {
+      // Hiện popup + overlay + blur nội dung chính
+      document.getElementById('language-popup').style.display = 'block';
+      document.getElementById('language-popup-overlay').style.display = 'block';
+      document.getElementById('main-content').classList.remove('language-selected');
+  }
+});
+
 function loadContent(lang) {
-  // Ẩn popup (giữ overlay + blur)
-  document.getElementById('language-popup').style.display = 'none';
-	document.getElementById('loading-media').classList.add('fade-in');
+// Ẩn popup (giữ overlay + blur)
+document.getElementById('language-popup').style.display = 'none';
+document.getElementById('loading-media').classList.add('fade-in');
 
+// Hiện GIF loading + thêm hiệu ứng fade-in
+const gif = document.getElementById('loading-media');
+gif.classList.add('fade-in');
+document.getElementById('loading-animation').style.display = 'block';
 
-  // Hiện GIF loading + thêm hiệu ứng fade-in
-  const gif = document.getElementById('loading-media');
-  gif.classList.add('fade-in');
-  document.getElementById('loading-animation').style.display = 'block';
+// Lấy tên repo tự động từ URL
+const repoName = window.location.pathname.split('/')[1];
 
-  // Chuyển trang sau 3 giây
-  setTimeout(() => {
-    window.location.href = `/${repoName}/${lang}/index.html`;
-  }, 3000);
+// Chuyển trang sau 3 giây
+setTimeout(() => {
+  // Đảm bảo redirect đúng với repoName và lang
+  window.location.href = `/${repoName}/${lang}/index.html`;
+}, 3000);
 }
